5de46665b8e13f07ba338a97b1c62026d5538432
vivtorochok <vika17052001@gmail.com>
2026-04-15 10:14:54 +0300

    up

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ const refs = {
 refs.formTask.addEventListener('submit', onAddBtnTaskSubmit);
 refs.listTask.addEventListener('change', onListTaskClick);
 refs.removeBtn.addEventListener('click', onRemoveBtnClick);
+refs.listTask.addEventListener('click', onListTaskClickDelete);
 function onAddBtnTaskSubmit(event) {
   event.preventDefault();
   const inputText = refs.inputTask.value.trim();
@@ -19,7 +20,7 @@ function onAddBtnTaskSubmit(event) {
                 <label class="task-label">
                 <input class="check" type="checkbox" />
                 <span class="task-text">${inputText}</span></label>
-                
+                <p class="remove-smile">✖️</p>
             </li>`;
 
   refs.listTask.insertAdjacentHTML('beforeend', liText);
@@ -42,4 +43,16 @@ function onRemoveBtnClick(event) {
   const allLi = document.querySelectorAll('.list-item');
   allLi.forEach(li => li.remove());
   refs.removeBtn.classList.add('btn-display-none');
+}
+
+function onListTaskClickDelete(event) {
+  if (event.target.nodeName !== 'P') {
+    return;
+  }
+  if (confirm('Бажаєте видалити це завдання?')) {
+    event.target.closest('.list-item').remove();
+  }
+  if (refs.listTask.children.length === 0) {
+    refs.removeBtn.classList.add('btn-display-none');
+  }
 }
